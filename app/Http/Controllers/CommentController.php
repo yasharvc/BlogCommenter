3f60ed25d337a3aebcd6e3b1a2784953dacba9e3
Yashar Aliabbasi <yasharvc@yahoo.com>
2022-01-23 19:10:56 +0300

    if parentid is wrong return error

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
+Use \Carbon\Carbon;
 
 class CommentController extends Controller
 {
@@ -11,6 +13,22 @@ class CommentController extends Controller
     }
 
     public function addComment(){
-
+        $parentId = request('parentId');
+        $username = request('username');
+        $comment = request('comment');
+        $pathToParent = '';
+        $parent_create_at = Carbon::now();
+        if($parentId != null){
+            var_dump($parentId);
+            $parent = Comment::find($parentId);
+            if($parent == null)
+            {
+                return array(
+                    'error'=>true,
+                    'code'=>400,
+                    'message'=>"Parent"
+                );
+            }
+        }
     }
 }
--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -8,16 +8,24 @@ use Tests\TestCase;
 
 class CommentTests extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_comments_get_should_exists()
     {
         $response = $this->get('/api/comments');
 
         $response->assertStatus(200);
     }
-    public function test_comment_add_endpoint_with_post_should_exists()
+    public function test_comment_add_endpoint_with_worng_parentId_should_return_400()
     {
-        $response = $this->post('/api/comment/add');
+        $response = $this->post('/api/comment/add',["parentId"=>"-1"]);
 
-        $response->assertStatus(200);
+        $response
+        ->assertStatus(200)
+        ->assertJson([
+            'error'=>true,
+            'code'=>400,
+            'message'=>"Parent"
+        ]);
     }
 }
