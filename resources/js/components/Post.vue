47eb837e5ed68edc6686be07278fd15c90482a50
Yashar Aliabbasi <yasharvc@yahoo.com>
2022-01-24 14:28:42 +0300

    Added root comments loading functions

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -49,12 +49,17 @@
                 :disabled="!isValid || inProgress"
                 @click="addComment"
               >
-                Add Comment<i v-if="inProgress" class="fa fa-spin fa-spinner"></i>
+                Add Comment<i
+                  v-if="inProgress"
+                  class="fa fa-spin fa-spinner"
+                ></i>
               </button>
             </div>
           </div>
         </div>
       </div>
+      <button @click="nextPage">Load comments</button>
+      {{ loadedComments }}
     </section>
   </div>
 </template>
@@ -64,15 +69,63 @@ export default {
     return {
       username: "Yashar",
       comment: "This is test",
+      loadedComments: [],
       inProgress: false,
+      pagination: {
+        current_page: 0,
+        per_page: 10,
+        last_page: 1,
+        next_page_url: null,
+      },
     };
   },
+  mounted() {},
   computed: {
     isValid() {
       return this.username.length > 0 && this.comment.length > 0;
     },
   },
   methods: {
+    nextPage() {
+      if (this.pagination.current_page < this.pagination.last_page) {
+        this.pagination.current_page++;
+        let url = this.pagination.next_page_url;
+        //First time or retry to get latest page
+        if (this.pagination.next_page_url == null) {
+          url = "/api/comments?page=" + this.current_page;
+        }
+        axios.get(url).then((resp) => {
+          console.warn(resp.data.data);
+          this.concatComments(resp.data.data);
+        });
+      } else {
+        alert("All comments already loaded");
+      }
+    },
+    concatComments(comments) {
+      this.makeTree(comments);
+    },
+    makeTree(comments) {
+      comments.forEach((e) => {
+        //If comment is a reply then path_to_parent has at least one comma(,)
+        if (e.path_to_parent.indexOf(',') > -1) {
+          
+        } else {
+          e.children = [];
+          this.loadedComments.push(e);
+        }
+      });
+      //return res;
+    },
+    getParent(all, input, path) {
+      var arr = path.split(",").slice(0, -1);
+      var res = null;
+      arr.forEach((id) => {
+        if (res == null) res = all.find((o) => o.id == parseInt(id));
+        else res = res.children.find((o) => o.id == parseInt(id));
+      });
+      return res;
+    },
     addComment() {
       this.inProgress = true;
       if (this.isValid) {
